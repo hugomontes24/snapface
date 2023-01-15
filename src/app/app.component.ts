5af262bf6775db96f1f3b97d029a9babb9de3585
hugo machado <hugomontes@hotmail.fr>
2023-01-15 15:59:02 +0100

    ngFor directive structurelle exercises.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,33 +7,33 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.mySnap = {
-      title: 'Archibald',
-      description: 'My best friend',
-      imageUrl: './../../assets/f2.jpg',
-      createdDate: new Date(),
-      snaps: 6,
-      location: 'Paris'
-    };
-    this.myOtherSnap = {
-      title: 'Archibald',
-      description: 'My best friend',
-      imageUrl: './../../assets/f1.jpg',
-      createdDate: new Date(),
-      snaps: 6,
-      location: 'Paris'
-    };
-    this.myLastSnap = {
-      title: 'Archibald',
-      description: 'My best friend',
-      imageUrl: './../../assets/f3.jpg',
-      createdDate: new Date(),
-      snaps: 6,
-    };
+    this.faceSnaps = [
+      {
+        title: 'Archibald',
+        description: 'My best friend',
+        imageUrl: './../../assets/f2.jpg',
+        createdDate: new Date(),
+        snaps: 6,
+        location: 'Paris'
+      },
+      {
+        title: 'Archibald',
+        description: 'My best friend',
+        imageUrl: './../../assets/f1.jpg',
+        createdDate: new Date(),
+        snaps: 6,
+        location: 'Paris'
+      },
+      {
+        title: 'Archibald',
+        description: 'My best friend',
+        imageUrl: './../../assets/f3.jpg',
+        createdDate: new Date(),
+        snaps: 6,
+      }
+      ];
   }
 }
