c183c70132309a56bed27b74ab3b07435781e1ba
hugo machado <hugomontes@hotmail.fr>
2023-01-19 15:01:23 +0100

    Observer Low level operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  interval$!: Observable<string>;
+
+  ngOnInit() {
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0),
+      map(value => value % 2 === 0 ?
+        `${value}, je suis pair` :
+        `${value}, je suis impair`),
+      tap(text => this.logger(text))
+    ); 
+  }
+
+  logger(text: string) {
+    console.log(text);
+  }
 }
