6d9ba01f17219f4f1a441e2c87991d1b9a75903b
hugo machado <hugomontes@hotmail.fr>
2023-01-21 12:25:36 +0100

    Observers. Memory leaks

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subject } from 'rxjs';
+import { take, takeUntil, tap } from 'rxjs/operators';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
 
@@ -7,14 +9,28 @@ import { FaceSnapsService } from '../services/face-snaps.service';
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
 
   faceSnaps!: FaceSnap[];
+  private destroy$!: Subject<boolean>;
 
   constructor(private faceSnapsService: FaceSnapsService) { }
 
   ngOnInit(): void {
     this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
+    this.destroy$ = new Subject<boolean>();
+    console.log(this.destroy$);
+
+    interval(1000).pipe(
+      //take(3),
+      takeUntil(this.destroy$),
+      tap(value => console.log(value))
+    ).subscribe();
   }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+
+}
 
 }
