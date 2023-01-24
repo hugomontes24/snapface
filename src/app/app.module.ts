749251a144232efbdecbd488f1cf5070420200f2
hugo machado <hugomontes@hotmail.fr>
2023-01-24 19:26:30 +0100

    Optimisation de l'architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,24 @@
-import { registerLocaleData } from '@angular/common';
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { FaceSnapComponent } from './face-snap/face-snap.component';
-import * as fr from '@angular/common/locales/fr';
-import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
-import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { SingleFaceSnapComponent } from './single-face-snap/single-face-snap.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NewFaceSnapComponent } from './new-face-snap/new-face-snap.component';
-import { HttpClientModule } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
+import { LandingPageModule } from './landing-page/landing-page.module';
+import { FaceSnapsModule } from './face-snaps/face-snaps.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FaceSnapComponent,
-    FaceSnapListComponent,
-    HeaderComponent,
-    LandingPageComponent,
-    SingleFaceSnapComponent,
-    NewFaceSnapComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
-  ],
-  providers: [
-    { provide: LOCALE_ID, useValue: 'fr-FR' }
+    CoreModule,
+    LandingPageModule,
+    FaceSnapsModule
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor() {
-    registerLocaleData(fr.default);
-  }
+
 }
--- src/app/face-snaps/components/new-face-snap/new-face-snap.component.ts
+++ src/app/face-snaps/components/new-face-snap/new-face-snap.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { map, Observable, tap } from 'rxjs';
-import { FaceSnap } from '../models/face-snap.model';
-import { FaceSnapsService } from '../services/face-snaps.service';
+import { FaceSnap } from './../../../core/models/face-snap.model';
+import { FaceSnapsService } from './../../../core/services/face-snaps.service';
 
 @Component({
   selector: 'app-new-face-snap',
@@ -44,8 +44,7 @@ export class NewFaceSnapComponent implements OnInit {
   onSubmitForm(): void {
     this.faceSnapsService.addFaceSnap(this.snapForm.value).pipe(
       tap(() => this.router.navigateByUrl('/facesnaps'))
-    ).subscribe();
-    
+    ).subscribe(); 
   }
 
 
